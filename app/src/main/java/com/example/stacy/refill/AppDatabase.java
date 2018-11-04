174a729b5ff95ad660a23f8e694b1e179f386b15
cherneha <staceychernega@gmail.com>
2018-11-05 00:05:22 +0200

    database instance singleton

---
+++ app/src/main/java/com/example/stacy/refill/AppDatabase.java
@@ -0,0 +1,11 @@
+package com.example.stacy.refill;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+@Database(entities = {Product.class}, version = 1, exportSchema = false)
+@TypeConverters({DateConverter.class})
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract ProductDao productDao();
+}
---
+++ app/src/main/java/com/example/stacy/refill/Database.java
@@ -0,0 +1,26 @@
+package com.example.stacy.refill;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+
+public class Database {
+    private static Database database;
+    private AppDatabase appDatabase;
+
+    private Database(Context context){
+        appDatabase = Room.databaseBuilder(context, AppDatabase.class, "database")
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public static Database getInstance(Context context){
+        if(database == null){
+            database = new Database(context);
+        }
+        return database;
+    }
+
+    public AppDatabase getAppDatabase(){
+        return appDatabase;
+    }
+}
