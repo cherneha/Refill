b62a57dca0b275a6e30fd944ae8df834e5681b42
cherneha <staceychernega@gmail.com>
2018-11-04 16:18:49 +0200

    added Product Entity & Room dependencies

---
+++ app/src/main/java/com/example/stacy/refill/Product.java
@@ -0,0 +1,70 @@
+package com.example.stacy.refill;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Product {
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private String name;
+
+    private double currentQuantity;
+
+    private double dailyUpdate;
+
+    private Date lastUpdate;
+
+    private double lastUpdateQuantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getCurrentQuantity() {
+        return currentQuantity;
+    }
+
+    public void setCurrentQuantity(double currentQuantity) {
+        this.currentQuantity = currentQuantity;
+    }
+
+    public double getDailyUpdate() {
+        return dailyUpdate;
+    }
+
+    public void setDailyUpdate(double dailyUpdate) {
+        this.dailyUpdate = dailyUpdate;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public double getLastUpdateQuantity() {
+        return lastUpdateQuantity;
+    }
+
+    public void setLastUpdateQuantity(double lastUpdateQuantity) {
+        this.lastUpdateQuantity = lastUpdateQuantity;
+    }
+}
