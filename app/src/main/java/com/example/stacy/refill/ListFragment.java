8a6e8107107a727b112547b3b90cd9617e86fbea
HGalitska <h.galitskaya@gmail.com>
2018-11-21 15:37:26 +0200

    changed list item layout

--- app/src/main/java/com/example/stacy/refill/ListFragment.java
+++ app/src/main/java/com/example/stacy/refill/ListFragment.java
@@ -1,5 +1,6 @@
 package com.example.stacy.refill;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -11,16 +12,21 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 public class ListFragment extends Fragment {
     LinearLayout listOfProducts;
+    
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,43 +34,79 @@ public class ListFragment extends Fragment {
         listOfProducts = view.findViewById(R.id.product_list);
         LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         linLayoutParam.gravity = Gravity.CENTER_HORIZONTAL;
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             addBlock("Olive Oil", "1.2");
         }
         return view;
     }
 
-    public void addBlock(String inputName, String productQuantity){
+    public void addBlock(String inputName, String productQuantity) {
 
         LinearLayout block = new LinearLayout(getActivity().getApplicationContext());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
         if (Build.VERSION.SDK_INT > 15) {
-            GradientDrawable shape =  new GradientDrawable();
-            shape.setCornerRadius( 12 );
+            GradientDrawable shape = new GradientDrawable();
+            shape.setCornerRadius(12);
             shape.setColor(Color.parseColor("#FFE0B2"));
             block.setBackground(shape);
-        }
-        else{
+        } else {
             block.setBackgroundColor(Color.parseColor("#FFE0B2"));
         }
 
         block.setOrientation(LinearLayout.VERTICAL);
-        params.setMargins(8,8,8,0);
+        params.setMargins(8, 8, 8, 0);
         block.setLayoutParams(params);
 
         LinearLayout nameLayout = new LinearLayout(getActivity().getApplicationContext());
         nameLayout.setOrientation(LinearLayout.HORIZONTAL);
         LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         if (Build.VERSION.SDK_INT > 15) {
-            GradientDrawable shape =  new GradientDrawable();
+            GradientDrawable shape = new GradientDrawable();
             shape.setCornerRadius(12);
             shape.setColor(getResources().getColor(R.color.colorPrimary));
             nameLayout.setBackground(shape);
-        }
-        else{
+        } else {
             nameLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         }
+
+        ImageButton editButton = new ImageButton(getActivity().getApplicationContext());
+        if (Build.VERSION.SDK_INT > 15) {
+            Drawable editIcon = getActivity().getResources().getDrawable(R.drawable.edit);
+            editIcon.setBounds(0, 0, 50, 50);
+            editButton.setBackground(editIcon);
+
+        } else {
+            editButton.setBackgroundColor(Color.parseColor("#FF9800"));
+        }
+        editButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        ImageButton deleteButton = new ImageButton(getActivity().getApplicationContext());
+        if (Build.VERSION.SDK_INT > 15) {
+            Drawable deleteIcon = getActivity().getResources().getDrawable(R.drawable.delete);
+            deleteIcon.setBounds(0, 0, 50, 50);
+            deleteButton.setBackground(deleteIcon);
+        } else {
+            deleteButton.setBackgroundColor(Color.parseColor("#FF9800"));
+        }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Do something in response to button click
+            }
+        });
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(80, 80);
+        buttonParams.setMargins(0, 0, 0, 0);
+        deleteButton.setLayoutParams(buttonParams);
+
+        LinearLayout.LayoutParams buttonParams2 = new LinearLayout.LayoutParams(80, 80);
+        buttonParams2.setMargins(650, 0, 0, 0);
+        editButton.setLayoutParams(buttonParams2);
+
 
 //        Button menuButton = new Button(getActivity().getApplicationContext());
 //
@@ -78,45 +120,58 @@ public class ListFragment extends Fragment {
 //        }
 //        menuButton.setGravity(Gravity.RIGHT);
 
+
         TextView text = new TextView(getActivity().getApplicationContext());
         text.setText(inputName);
-        text.setPadding(8,8,8,8);
+        text.setPadding(8, 8, 8, 8);
         text.setTextSize(20);
         nameLayout.setLayoutParams(params2);
+        text.setGravity(Gravity.LEFT);
         nameLayout.addView(text);
+//        nameLayout.addView(menuButton);
+
+        nameLayout.addView(editButton);
+        nameLayout.addView(deleteButton);
         block.addView(nameLayout);
 
         LinearLayout quantityLayout = new LinearLayout(getActivity().getApplicationContext());
         quantityLayout.setOrientation(LinearLayout.HORIZONTAL);
 
-        Button basket = new Button(getActivity().getApplicationContext());
-
+        ImageButton addButton = new ImageButton(getActivity().getApplicationContext());
         if (Build.VERSION.SDK_INT > 15) {
-            Drawable bag = getActivity().getResources().getDrawable(R.drawable.cart);
-            bag.setBounds(0, 0, 50, 50);
-            basket.setBackground(bag);
+            Drawable cartIcon = getActivity().getResources().getDrawable(R.drawable.add);
+            cartIcon.setBounds(0, 0, 50, 50);
+            addButton.setBackground(cartIcon);
+        } else {
+            addButton.setBackgroundColor(Color.parseColor("#FF9800"));
         }
-        else {
-            basket.setBackgroundColor(Color.parseColor("#FF9800"));
-            basket.setText("Add");
-        }
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        LinearLayout.LayoutParams addButtonParams = new LinearLayout.LayoutParams(80, 80);
+        addButtonParams.setMargins(0, 20, 0, 0);
+        addButton.setLayoutParams(addButtonParams);
 
 
         TextView quantity = new TextView(getActivity().getApplicationContext());
         quantity.setText(productQuantity);
         quantity.setTextSize(30);
-        quantity.setPadding(16, 8, 16,8 );
+        quantity.setPadding(16, 8, 16, 0);
         quantityLayout.addView(quantity);
-//        quantityLayout.addView(basket);
-
-        View view = new View(getActivity().getApplicationContext());
-        LinearLayout.LayoutParams separatorParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 4);
+        quantityLayout.addView(addButton);
 
 
+//        View view = new View(getActivity().getApplicationContext());
+//        LinearLayout.LayoutParams separatorParams = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT, 4);
+//
+//
 //        separatorParams.setMargins(8, 0, 8, 4);
-        view.setLayoutParams(separatorParams);
-        view.setBackgroundColor(Color.parseColor("#3E2723"));
+//        view.setLayoutParams(separatorParams);
+//        view.setBackgroundColor(Color.parseColor("#3E2723"));
 //        block.addView(view);
         block.addView(quantityLayout);
 
