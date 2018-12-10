d1c6cbe0deda0ad88a23cd5f28f39a4807dfc0a5
cherneha <staceychernega@gmail.com>
2018-12-10 13:26:40 +0200

    added notofication time setting

--- app/src/main/java/com/example/stacy/refill/SettingsPreferences.java
+++ app/src/main/java/com/example/stacy/refill/SettingsPreferences.java
@@ -1,15 +1,16 @@
 package com.example.stacy.refill;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class SettingsPreferences {
     SharedPreferences settingsPreferences;
-    Activity activity;
-    public SettingsPreferences(Activity activity){
-        this.activity = activity;
-        settingsPreferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
+    Context context;
+    public SettingsPreferences(Context context){
+        this.context = context;
+        settingsPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     public int getDaysBeforeRunout(){
@@ -20,5 +21,25 @@ public class SettingsPreferences {
         SharedPreferences.Editor editor = settingsPreferences.edit();
         editor.putInt("days_before_runout", days);
         editor.apply();
+    }
+
+    public void setNotificationHour(int hour){
+        SharedPreferences.Editor editor = settingsPreferences.edit();
+        editor.putInt("notification_hour", hour);
+        editor.apply();
+    }
+
+    public void setNotificationMinutes(int minutes){
+        SharedPreferences.Editor editor = settingsPreferences.edit();
+        editor.putInt("notification_minute", minutes);
+        editor.apply();
+    }
+
+    public int getNotificationHour(){
+        return settingsPreferences.getInt("notification_hour", 9);
+    }
+
+    public int getNotificationMinutes(){
+        return settingsPreferences.getInt("notification_minute", 0);
     }
 }
--- app/src/main/java/com/example/stacy/refill/ToolsFragment.java
+++ app/src/main/java/com/example/stacy/refill/ToolsFragment.java
@@ -7,10 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class ToolsFragment extends Fragment {
     SettingsPreferences settingsPreferences;
     EditText daysBeforeRunoutEdit;
+    TimePicker notificationTime;
 
 
     @Nullable
@@ -18,6 +20,9 @@ public class ToolsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tools_fragment, container, false);
         settingsPreferences = new SettingsPreferences(getActivity());
+        notificationTime = view.findViewById(R.id.notification_time);
+        notificationTime.setHour(settingsPreferences.getNotificationHour());
+        notificationTime.setMinute(settingsPreferences.getNotificationMinutes());
         daysBeforeRunoutEdit = view.findViewById(R.id.days_before_runout);
         daysBeforeRunoutEdit.setText(String.valueOf(settingsPreferences.getDaysBeforeRunout()));
         return view;
@@ -28,5 +33,7 @@ public class ToolsFragment extends Fragment {
         super.onPause();
         int newDaysBeforeRunout  = Integer.valueOf(daysBeforeRunoutEdit.getText().toString());
         settingsPreferences.setDaysBeforeRunout(newDaysBeforeRunout);
+        settingsPreferences.setNotificationHour(notificationTime.getHour());
+        settingsPreferences.setNotificationMinutes(notificationTime.getMinute());
     }
 }
